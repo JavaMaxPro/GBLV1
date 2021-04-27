9d655bfc437316578cbc9f11473107281f64294a
Maksim <mgubkov@gmail>
2021-04-28 00:10:22 +0700

    lesson 4 Turn human & computer

--- src/ru/gb/level1/lessons4/TikTokToe.java
+++ src/ru/gb/level1/lessons4/TikTokToe.java
@@ -2,6 +2,8 @@ package ru.gb.level1.lessons4;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class TikTokToe {
     private static final int SIZE = 3;
@@ -10,7 +12,7 @@ public class TikTokToe {
     private static final char DOT_X = 'X';
     private static final char DOT_0 = 'O';
 
-    private static char[][] map;
+    private static char[][] map = new char[SIZE][SIZE];
 
 
     public static void main(String[] args) {
@@ -19,25 +21,73 @@ public class TikTokToe {
 
     }
 
+    private static void humanTurn() {
+        int x ;
+        int y ;
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.println("Введите координату x y");
+            x = readInt(scan);
+            y = readInt(scan);
+
+            if (x == -1 || y == -1) {
+                System.out.println("Координаты должны быть числом");
+                scan.nextLine();
+            } else if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+                System.out.println("Данные ведены не корректно!");
+            } else if (map[x][y] != DOT_EMPTY) {
+                System.out.println("Клетка уже занята!");
+            } else {
+                break;
+            }
+        } while (true);
+        map[x][y] = DOT_X;
+    }
+
+    private static void computerTurn() {
+        int x;
+        int y;
+        Random random = new Random();
+
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (map[x][y] != DOT_EMPTY);
+        map[x][y] = DOT_0;
+    }
+
+    private static int readInt(Scanner scan) {
+        return scan.hasNextInt() ? scan.nextInt() - 1 : -1;
+    }
+
     private static void printMap() {
-        for (int i = 0; i <= SIZE; i++) {
-            System.out.print((i)+" ");
-        }
+
+        printHeader();
+        printBody();
         System.out.println();
+    }
+
+    private static void printBody() {
         for (int i = 0; i < SIZE; i++) {
-            System.out.print((i+1)+" ");
+            System.out.print((i + 1) + " ");
             for (int j = 0; j < SIZE; j++) {
-                System.out.print(map[i][j]+ " ");
+                System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
     }
 
+    private static void printHeader() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print((i) + " ");
+        }
+        System.out.println();
+    }
+
 
     private static void initMap() {
-        map= new char[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
-            Arrays.fill(map[i],DOT_EMPTY);
+            Arrays.fill(map[i], DOT_EMPTY);
         }
     }
 }
