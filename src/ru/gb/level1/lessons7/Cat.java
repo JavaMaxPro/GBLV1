d52530903c27fc8bc0eba1be684e45690c9f690a
Maksim <mgubkov@gmail>
2021-05-06 16:43:17 +0700

    lesson 7

---
+++ src/ru/gb/level1/lessons7/Cat.java
@@ -0,0 +1,32 @@
+package ru.gb.level1.lessons7;
+
+public class Cat {
+
+    private String name;
+    private int appetite;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate p) {
+        p.decreaseFood(appetite);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+}
---
+++ src/ru/gb/level1/lessons7/Plate.java
@@ -0,0 +1,23 @@
+package ru.gb.level1.lessons7;
+
+public class Plate {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+}
