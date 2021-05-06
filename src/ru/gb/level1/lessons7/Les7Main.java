915d32bd969ba21a637265a7cfd92e4e6c0fb07d
Maksim <mgubkov@gmail>
2021-05-06 17:18:36 +0700

    lesson 7 Task 1,2,3,4,5,6

--- src/ru/gb/level1/lessons7/Les7Main.java
+++ src/ru/gb/level1/lessons7/Les7Main.java
@@ -4,10 +4,22 @@ public class Les7Main {
     public static void main(String[] args) {
 
         Cat cat = new Cat("Barsik", 5);
-        Plate plate = new Plate(100);
+        Plate plate = new Plate(5);
         plate.info();
         cat.eat(plate);
         plate.info();
 
+        plate.addFood(38);
+        Cat [] masCat = new Cat[5];
+        masCat[0] = new Cat("Murzic", 4);
+        masCat[1] = new Cat("Myau", 15);
+        masCat[2] = new Cat("Rizik", 10);
+        masCat[3] = new Cat("Syam", 6);
+        masCat[4] = new Cat("Lisa", 5);
+
+        for (Cat value : masCat) {
+            value.eat(plate);
+            plate.info();
+        }
     }
 }
