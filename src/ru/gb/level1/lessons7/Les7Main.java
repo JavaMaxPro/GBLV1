3b6c4afa85b41762ae1d1ef724182e3903cd2dde
Maksim <mgubkov@gmail>
2021-05-06 22:54:10 +0700

    lesson 7 Task 1,2,3,4,5,6 + Task **

--- src/ru/gb/level1/lessons7/Les7Main.java
+++ src/ru/gb/level1/lessons7/Les7Main.java
@@ -1,5 +1,7 @@
 package ru.gb.level1.lessons7;
 
+import java.util.Locale;
+
 public class Les7Main {
     public static void main(String[] args) {
 
@@ -10,7 +12,7 @@ public class Les7Main {
         plate.info();
 
         plate.addFood(38);
-        Cat [] masCat = new Cat[5];
+        Cat[] masCat = new Cat[5];
         masCat[0] = new Cat("Murzic", 4);
         masCat[1] = new Cat("Myau", 15);
         masCat[2] = new Cat("Rizik", 10);
@@ -21,5 +23,19 @@ public class Les7Main {
             value.eat(plate);
             plate.info();
         }
+
+        enterText("I like Java!!!");
+    }
+
+    public static void enterText(String text) {
+        System.out.println(text.charAt(text.length() - 1));
+        if (text.endsWith("!!!")) System.out.println("Заканчивается на !!!");
+        if (text.startsWith("I like")) System.out.println("Начинается на I like");
+        if (text.contains("Java")) System.out.println("Содержит слово Java в тексте");
+        System.out.println("Позиция Java = " + text.indexOf("Java"));
+        System.out.println(text.replace('a', 'o'));
+        System.out.println(text.toLowerCase(Locale.ROOT));
+        System.out.println(text.toUpperCase(Locale.ROOT));
+        System.out.println((text.substring(0, text.indexOf("Java")) + text.substring(text.indexOf("Java") + 4)));
     }
 }
